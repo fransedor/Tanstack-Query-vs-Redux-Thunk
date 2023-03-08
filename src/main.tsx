80dd519a3537465285b120e3c7ef19a591283a68
Fransedo Ravelino <fransedo.ravelino@BLUEBIRDGROUP.COM>
2023-03-08 13:47:35 +0700

    added query client

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query/build/lib/QueryClientProvider";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      staleTime: 86400,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
+  </QueryClientProvider>
 );
